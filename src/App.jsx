b53d56da29360f13ddaee25cf4e4569fdb55ebae
Sonick Mumba <kingellie.mumba@gmail.com>
2023-11-04 01:15:31 +0200

    Implement qr-code from frontend mentor

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="qr-main-div">
+      <img src="" className="img-tag"/>
+      <div className="qr-heading-div">
+        <p className="main-heading">Improve your front-end skills by building projects</p>
+        <p className="bottom-heading">
+          Scan the QR code to visit Frontend mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
